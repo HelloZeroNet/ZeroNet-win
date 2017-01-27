33c6cce958576af1bd0113bb334199de84f65060
shortcutme <tamas@zeronet.io>
2017-01-27 12:10:06 +0100

    Update ZeroNet source code

--- core/src/Config.py
+++ core/src/Config.py
@@ -10,7 +10,7 @@ class Config(object):
 
     def __init__(self, argv):
         self.version = "0.5.1"
-        self.rev = 1839
+        self.rev = 1840
         self.argv = argv
         self.action = None
         self.config_file = "zeronet.conf"
