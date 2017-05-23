6efb52336ad0ee15bc64abb4616b88ee9c68ab29
shortcutme <tamas@zeronet.io>
2017-05-23 17:13:52 +0200

    Update ZeroNet source code

--- core/src/Config.py
+++ core/src/Config.py
@@ -10,7 +10,7 @@ class Config(object):
 
     def __init__(self, argv):
         self.version = "0.5.5"
-        self.rev = 2081
+        self.rev = 2087
         self.argv = argv
         self.action = None
         self.config_file = "zeronet.conf"
@@ -187,7 +187,8 @@ class Config(object):
                                  metavar='address')
         self.parser.add_argument('--updatesite', help='Source code update site', default='1UPDatEDxnvHDo7TXvq6AEBARfNkyfxsp',
                                  metavar='address')
-        self.parser.add_argument('--size_limit', help='Default site size limit in MB', default=10, type=int, metavar='size')
+        self.parser.add_argument('--size_limit', help='Default site size limit in MB', default=10, type=int, metavar='limit')
+        self.parser.add_argument('--file_size_limit', help='Maximum per file size limit in MB', default=10, type=int, metavar='limit')
         self.parser.add_argument('--connected_limit', help='Max connected peer per site', default=8, type=int, metavar='connected_limit')
         self.parser.add_argument('--workers', help='Download workers per site', default=5, type=int, metavar='workers')
 
